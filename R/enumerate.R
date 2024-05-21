8c2c3dec89371fadffa7eedb3d87492c604aee9b
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-05-21 07:55:09 +0000

    Add a new script based on the old minionator README.

--- R/enumerate.R
+++ R/enumerate.R
@@ -1,12 +1,36 @@
 library(dplyr)
 library(minionator)
 
-# this is already broken
+discrete_matrix <- function(nrow, ncol, domain, name = "l") {
+  tidyr::expand_grid(row = 0:(nrow - 1), col = 0:(ncol - 1)) |>
+    dplyr::mutate(
+      lower = min(domain),
+      upper = max(domain),
+      name  = name,
+      type  = "DISCRETE"
+    )
+}
+
 l <- discrete_matrix(3, 3, 0:2)
 
 row_latin <- tribble(
-  ~constraint,             ~variables,
-  "alldiff", l |> filter(row == 0),
-  "alldiff", l |> filter(row == 1),
-  "alldiff", l |> filter(row == 2)
+  ~constraint,            ~variables,
+    "alldiff", l |> filter(row == 0),
+    "alldiff", l |> filter(row == 1),
+    "alldiff", l |> filter(row == 2)
 )
+
+column_latin <- tribble(
+  ~constraint,            ~variables,
+    "alldiff", l |> filter(col == 0),
+    "alldiff", l |> filter(col == 1),
+    "alldiff", l |> filter(col == 2)
+)
+
+latin <- list(
+          variables = l,
+             search = "PRINT ALL",
+  unary_constraints = bind_rows(row_latin, column_latin)
+)
+
+minion_output(latin)
